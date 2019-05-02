6861285c213a4e6284692b71a9532869af83e251
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2019-05-02 17:23:01 -0500

    launch

--- ResumeApp/Utils/RoundableButton.swift
+++ ResumeApp/Utils/RoundableButton.swift
@@ -35,26 +35,6 @@ class RoundableButton: UIButton {
         }
     }
     
-    @IBInspectable var jibaroTint: Bool = false {
-        didSet{
-            self.colorJibaro()
-        }
-    }
-    
-    func colorJibaro() {
-        
-        let layerGradient = CAGradientLayer()
-        
-        // Drawing code
-        layerGradient.colors = [UIColor(displayP3Red: 24/255, green: 42/255, blue: 115/255, alpha: 1).cgColor, UIColor(displayP3Red: 166/255, green: 64/255, blue: 31/255, alpha: 1).cgColor]
-        layerGradient.startPoint = CGPoint(x: 0, y: 0.5)
-        layerGradient.endPoint = CGPoint(x: 1, y: 0.5)
-        layerGradient.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height)
-        
-        self.layer.insertSublayer(layerGradient, at: 0)
-        
-    }
-    
     func applyCornerRadius()
     {
         if(self.circular) {
